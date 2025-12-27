d77785af9e723dab40086db6d2fab322c28a1a68
RicardorDuarte <a2022137878@isec.pt>
2025-12-27 16:29:22 +0000

    coisas

--- GestaoLoja/AppConfig.cs
+++ GestaoLoja/AppConfig.cs
@@ -3,7 +3,7 @@
     public static class AppConfig
     {
         public static readonly string tituloHomePage = "MyMEDIA";
-        public static readonly string enderecohome = "Rua";
+        public static readonly string enderecohome = "Rua dos carvalhos nº 25 esquerdo";
         public static readonly string mail = "teste@teste.com";
         public static readonly string telefone = "+351 911 111 111";
     }
